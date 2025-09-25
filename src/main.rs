b80ad3d018b6cb17b729f4f239e3399de2eaab34
nampc <namphamc99@gmail.com>
2025-09-26 01:20:35 +0700

    wip: finite field arithmetic

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+use num_bigint::BigUint;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct FieldElement {
+    num: BigUint,
+    prime: BigUint,
+}
+
+impl FieldElement {
+    pub fn new(num: BigUint, prime: BigUint) -> Result<Self, String> {
+        if num >= prime {
+            return Err(format!(
+                "Num {} is not in field range 0 to {}",
+                num,
+                &prime - 1u32 // prime - 1u32 would also work thanks to "auto borrowing"
+            ));
+        }
+
+        if prime <= BigUint::from(1u32) {
+            return Err("Invalid prime".to_string());
+        }
+
+        Ok(FieldElement { num, prime })
+    }
+
+    pub fn pow(exponent: BigUint) -> Result<Self, String> {
+        
+    }
+}
+
+impl Add for FieldElement {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            panic!("Two elements are not in the same field")
+        }
+
+        let num = (self.num + rhs.num) % &self.prime;
+
+        FieldElement {
+            num,
+            prime: self.prime,
+        }
+    }
+}
+
+impl Sub for FieldElement {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            panic!("Two elements are not in the same field")
+        }
+
+        // adding prime before subtracting to make sure the result is greater than 0
+        let num = (self.num + &self.prime - rhs.num) % &self.prime;
+
+        FieldElement {
+            num,
+            prime: self.prime,
+        }
+    }
+}
+
+impl Mul for FieldElement {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            panic!("Two elements are not in the same field")
+        }
+
+        let num = (self.num * rhs.num) % &self.prime;
+
+        FieldElement {
+            num,
+            prime: self.prime
+        }
+    }
+}
+
+impl Div for FieldElement {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            panic!("Two elements are not in the same field")
+        }
+
+        let num = (self.num / rhs.num) % &self.prime;
+
+        FieldElement {
+            num,
+            prime: self.prime
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
